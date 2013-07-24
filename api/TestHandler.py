1f5bd876c0eab03edbf9a66393b0817c17d4690c
Kevin Hsu <k.wk.hsu@gmail.com>
2013-07-24 03:38:14 -0400

    Initial commit

---
+++ api/TestHandler.py
@@ -0,0 +1,31 @@
+import tornado.web
+import json
+
+"""
+TO DO:
+BaseHandler class
+- standard get/put/post/delete methods
+- debug info decorator
+
+Models 
+- define SQLAlchemy models for objects
+- pass model to handler?
+- how could we design that way?
+"""
+
+
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        qs_args = self.request.arguments
+	if len(qs_args) == 0:
+		self.write('empty query string')
+	else:
+		self.write(query)
+
+
+    def post(self):
+        data = json.loads(self.request.body)
+        
+        for key, value in data.iteritems():
+            self.write(key + " " + value + "\n")
+        
---
+++ server.py
@@ -0,0 +1,23 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from api.TestHandler import *
+
+from tornado.options import define, options
+define("port", default=8000)
+
+class ApiApplication(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/test", TestHandler)
+        ]
+        
+        tornado.web.Application.__init__(self, handlers, debug=True)
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(ApiApplication())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
